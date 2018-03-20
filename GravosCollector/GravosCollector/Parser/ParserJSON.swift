c09389c273fa8ce5bcb5bda01a8ac03e20cea018
Abhijit <abhijit@etechnologycentre.com>
2018-03-20 12:55:48 +0800

    Added Parser Class

---
+++ GravosCollector/GravosCollector/Parser/ParserJSON.swift
@@ -0,0 +1,13 @@
+//
+//  ParserJSON.swift
+//  GravosCollector
+//
+//  Created by Abhijit on 20/3/18.
+//  Copyright © 2018 Abhijit. All rights reserved.
+//
+
+import UIKit
+
+class ParserJSON: NSObject {
+
+}
