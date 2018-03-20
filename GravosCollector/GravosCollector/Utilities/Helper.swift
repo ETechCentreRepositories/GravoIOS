6d29e3ce080a8fcb0650b05a812ea32db4af4d7e
Abhijit <abhijit@etechnologycentre.com>
2018-03-20 12:52:11 +0800

    Adding Helper Class

---
+++ GravosCollector/GravosCollector/Utilities/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  GravosCollector
+//
+//  Created by Abhijit on 20/3/18.
+//  Copyright © 2018 Abhijit. All rights reserved.
+//
+
+import UIKit
+
+class Helper: NSObject
+{
+
+}
